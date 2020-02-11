fcdbe7d7565cd7b6f6189ae5902c2ff10d693780
John Holcroft <john@montreux.co.uk>
2020-02-11 12:03:50 +0000

    Updated lodash dependancy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Container from '@material-ui/core/Container';
 
-import { withAuthenticator } from 'aws-amplify-react'; // or 'aws-amplify-react-native';
-import Amplify from 'aws-amplify';
-import awsconfig from './aws-exports'; // if you are using Amplify CLI
+import Amplify, { Hub, Auth } from 'aws-amplify';
+import awsconfig from './aws-exports'; 
 
 import Shooters from './Shooters';
 import CampHelpers from './CampHelpers';
@@ -12,13 +11,48 @@ import CampingSummary from './CampingSummary';
 import EmergencyContacts from './EmergencyContacts';
 import Permissions from './Permissions';
 
+Amplify.configure(awsconfig);
+
 function App() {
 
-  Amplify.configure(awsconfig);
-  
+  const [state, setState] = useState({ user: null, customState: null });
+
+  useEffect(() => {
+    Hub.listen("auth", ({ payload: { event, data } }) => {
+      switch (event) {
+        case "signIn":
+          this.setState({ user: data });
+          break;
+        case "signOut":
+          this.setState({ user: null });
+          break;
+        case "customOAuthState":
+          this.setState({ customState: data });
+      }
+    })
+  });
+
+    Auth.currentAuthenticatedUser()
+      .then(user => this.setState({ user }))
+      .catch(() => console.log("Not signed in"));
+      
+  const handleSignIn = () => {
+    Auth.federatedSignIn().then(cred => {
+      // If success, you will get the AWS credentials
+      console.log(cred);
+      return Auth.currentAuthenticatedUser();
+      }).then(user => {
+        // If success, the user object you passed in Auth.federatedSignIn
+        console.log(user);
+        }).catch(e => {
+          console.log(e)
+        })
+  };
+
   return (
     <div className="App">
-    
+    <button onClick={() => 
+    Auth.federatedSignIn().then({ handleSignIn })}>Sign In</button>
     <Container maxWidth="sm">
       <Shooters/>
       <CampHelpers/>
@@ -27,9 +61,8 @@ function App() {
       <Permissions/>
     </Container>
         
-        
     </div>
   );
 }
 
-export default withAuthenticator(App, {includeGreetings: true});
+export default App;
