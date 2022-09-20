73e63ce55608f4888f82f8e5286a2e6c595e25e0
John Holcroft <john.holcroft@montreux.co.uk>
2022-09-20 09:53:53 +0100

    Example code used to generate CSV for all entrants.
    Now replaced by button in the web site.

---
+++ src/AllEntriesData2022.ts
@@ -0,0 +1,68 @@
+export const allEntriesSep19 = [
+  {
+    owner: 'abc@yahoo.co.uk',
+    updated: '2022-09-17T20:45:08.327Z',
+    id: 'abc@yahoo.co.uk-submitted-2022',
+    teamEntry: {
+      allEntries: [
+        {
+          shooter: {
+            id: 'Joe-Bloggs-Woking Explorers',
+            firstName: 'Joe',
+            lastName: 'Bloggs',
+            previousCompetitorNumber: '',
+            dateOfBirth: '2007-08-27T23:00:00.000Z',
+            scoutGroup: 'Woking Explorers',
+            county: '',
+            isRangeOfficer: false,
+            rangeOfficerProofUrl: '',
+          },
+          enteredEventIds: [
+            'knockout',
+            'airrifle6yd',
+            'airpistol6yd',
+            'fieldTarget',
+            'ownrifle10msporter',
+            'ownpistol',
+          ],
+        },
+        {
+          shooter: {
+            id: 'Jane-Doe-Aviators Network',
+            firstName: 'Jane',
+            lastName: 'Doe',
+            previousCompetitorNumber: '123',
+            dateOfBirth: '1900-01-01T00:00:01.000Z',
+            scoutGroup: 'Aviators Network',
+            county: '',
+            isRangeOfficer: false,
+            rangeOfficerProofUrl: '',
+          },
+          enteredEventIds: [
+            'knockout',
+            'airrifle6yd',
+            'airpistol6yd',
+            'fieldTarget',
+            'ownpistol',
+            'smallbore',
+            'ownrifle10msporter',
+          ],
+        },
+      ],
+      campBooking: {
+        numberOfCampers: 0,
+        estimatedArrivalTime: '',
+        anyOtherInfo: '',
+      },
+      onSiteEmergencyContact: {
+        name: 'Jon Culshaw',
+        contactNumber: '07958 123456',
+      },
+      offSiteEmergencyContact: {
+        name: 'Jane Culshaw',
+        contactNumber: '07748 123456',
+      },
+    },
+    state: 'submitted',
+  },
+];
