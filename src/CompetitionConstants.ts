3c819e5ed2a03d09258cb531d6b8fca3aeec4c1f
John Holcroft <john.holcroft@montreux.co.uk>
2024-10-12 19:33:52 +0100

    Updated entry closing date

--- src/CompetitionConstants.ts
+++ src/CompetitionConstants.ts
@@ -2,7 +2,7 @@ export const CurrentCompetitionYear = '2024';
 export const CompetitionDate = Date.parse('2024-10-18T00:00Z');
 // Set the EntryClosingDate to 1 AM UTC on the day after the closing date,
 // which is midnight BST on the intended closing date.
-export const EntryClosingDate = new Date('2024-10-13T01:00:00Z');
+export const EntryClosingDate = new Date('2024-10-14T01:00:00Z');
 export const GracePeriodEntryClosingDate = new Date('2023-10-02T01:00:00Z');
 export const logoImage = 'NSRC 2024 Logo.svg';
 export const logoImageAltText = 'National Scout Rifle Postal Competition 2024';
